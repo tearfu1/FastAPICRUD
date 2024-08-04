c317f7ef6c0e4207b66256b524e697c72be32074
tearfu1 <axuior12@gmail.com>
2024-08-04 07:55:43 +0200

    refactored structure of the project and added sqlalchemyorm, postresql in docker-compose, alembic, config

---
+++ app/students/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import ForeignKey, text, Text
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from app.database import Base, str_uniq, int_pk, str_null_true
+from datetime import date
+
+
+# создаем модель таблицы студентов
+class Student(Base):
+    id: Mapped[int_pk]
+    phone_number: Mapped[str_uniq]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    date_of_birth: Mapped[date]
+    email: Mapped[str_uniq]
+    address: Mapped[str] = mapped_column(Text, nullable=False)
+    enrollment_year: Mapped[int]
+    course: Mapped[int]
+    special_notes: Mapped[str_null_true]
+    major_id: Mapped[int] = mapped_column(ForeignKey("majors.id"), nullable=False)
+
+    major: Mapped["Major"] = relationship("Major", back_populates="students")
+
+    def __str__(self):
+        return (f"{self.__class__.__name__}(id={self.id}, "
+                f"first_name={self.first_name!r},"
+                f"last_name={self.last_name!r})")
+
+    def __repr__(self):
+        return str(self)
+
+
+# создаем модель таблицы факультетов (majors)
+class Major(Base):
+    id: Mapped[int_pk]
+    major_name: Mapped[str_uniq]
+    major_description: Mapped[str_null_true]
+    count_students: Mapped[int] = mapped_column(server_default=text('0'))
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
+
+    def __repr__(self):
+        return str(self)
